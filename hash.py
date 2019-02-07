b6b6ebf47f86c66177367c97575da1f596528a84
Igor Alexanov <popzxc@yandex.ru>
2019-02-07 15:51:08 +0300

    Initial commit

---
+++ hash.py
@@ -0,0 +1,12 @@
+from Crypto.Hash import keccak
+
+def hash(data):
+    keccak_hash = keccak.new(digest_bits=256)
+    keccak_hash.update(data)
+    return keccak_hash.digest()
+
+
+def hex_hash(data):
+    keccak_hash = keccak.new(digest_bits=256)
+    keccak_hash.update(data)
+    return keccak_hash.hexdigest()
---
+++ mpt.py
@@ -0,0 +1,23 @@
+class NibblesPath:
+    def __init__(self, data, offset = 0):
+        self._data = data
+        self._offset = offset
+
+class Node:
+    class Leaf:
+        def __init__(self, path, data):
+            self.path = path
+            self.data = data
+
+    class Extension:
+        def __init__(self, path, next_ref):
+            self.path = path
+            self.next_ref = next_ref
+
+    class Branch:
+        def __init__(self, branches, data = None):
+            self.branches = branches
+            self.data = data
+
+    def __init__(self, node_inner):
+        self.inner = node_inner
