1db4848af41aeef4f46961b20ebc9f3e1baae1ab
Igor Alexanov <popzxc@yandex.ru>
2019-02-19 13:44:01 +0300

    Add test for MPT interface

--- tests/tests.py
+++ tests/tests.py
@@ -2,8 +2,8 @@ import unittest
 import mpt
 import rlp
 
+
 class TestNibblePath(unittest.TestCase):
-    
     def test_at(self):
         nibbles = mpt.NibblePath([0x12, 0x34])
         self.assertEqual(nibbles.at(0), 0x1)
@@ -19,8 +19,17 @@ class TestNibblePath(unittest.TestCase):
         with self.assertRaises(IndexError):
             nibbles.at(3)
 
-class TestNode(unittest.TestCase):
+    def test_encode(self):
+        nibbles = mpt.NibblePath([0x12, 0x34])
+        self.assertEqual(nibbles.encode(False), b'\x00\x12\x34')
+        self.assertEqual(nibbles.encode(True), b'\x20\x12\x34')
 
+        nibbles = mpt.NibblePath([0x12, 0x34], offset=1)
+        self.assertEqual(nibbles.encode(False), b'\x12\x34')
+        self.assertEqual(nibbles.encode(True), b'\x32\x34')
+
+
+class TestNode(unittest.TestCase):
     def assertRoundtrip(self, raw_node, expected_type):
         decoded = mpt.Node.decode(raw_node)
         encoded = decoded.encode()
@@ -33,8 +42,35 @@ class TestNode(unittest.TestCase):
         pass
 
     def test_leaf(self):
-        # Path 0xABC. 0x3 at the beginning: 0x2 (for leaf type) + 0x1 (for odd len) 
-        nibbles_path = [0x3A, 0xBC]
-        data = [0xDE, 0xAD, 0xBE, 0xEF]
+        # Path 0xABC. 0x3_ at the beginning: 0x20 (for leaf type) + 0x10 (for odd len)
+        nibbles_path = bytearray([0x3A, 0xBC])
+        data = bytearray([0xDE, 0xAD, 0xBE, 0xEF])
         raw_node = rlp.encode([nibbles_path, data])
         self.assertRoundtrip(raw_node, mpt.Node.Leaf)
+
+
+class TestMPT(unittest.TestCase):
+    def test_insert_get_one_short(self):
+        storage = {}
+        trie = mpt.MerklePatriciaTrie(storage)
+
+        key = rlp.encode(b'key')
+        value = rlp.encode(b'value')
+        trie.update(key, value)
+        gotten_value = trie.get(key)
+
+        self.assertEqual(value, gotten_value)
+
+        with self.assertRaises(KeyError):
+            trie.get(rlp.encode(b'no_key'))
+
+    def test_insert_get_one_long(self):
+        storage = {}
+        trie = mpt.MerklePatriciaTrie(storage)
+
+        key = rlp.encode(b'key_00000000000000000000000000000000')
+        value = rlp.encode(b'value_00000000000000000000000000000000')
+        trie.update(key, value)
+        gotten_value = trie.get(key)
+
+        self.assertEqual(value, gotten_value)
