43f7bc088e008542ce7530237bdbd0f028230ab6
Igor Alexanov <popzxc@yandex.ru>
2019-03-15 20:41:50 +0300

    Specify dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,15 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/popzxc/merkle-patricia-trie",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'cytoolz==0.9.0.1',
+        'eth-hash==0.2.0',
+        'eth-typing==2.0.0',
+        'eth-utils==1.4.1',
+        'pycryptodome==3.7.3',
+        'rlp==1.1.0',
+        'toolz==0.9.0',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
