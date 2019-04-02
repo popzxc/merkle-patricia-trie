cc2cebaf196745a6901028b0c84eb1abcbb2f81f
Igor Alexanov <popzxc@yandex.ru>
2019-04-02 09:11:47 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="eth_mpt",
-    version="0.1.0",
+    version="0.2.0",
     author="Igor Aleksanov",
     author_email="popzxc@yandex.com",
     description="A simlpe Merkle Patricia Trie implementation",
